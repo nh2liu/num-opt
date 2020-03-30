ffff0f7d09d5d30c8f557c2b609b5c7764d9c2f0
Neil Liu <neil.liu16@gmail.com>
2020-03-30 12:30:36 +0800

    Autograd1 (#1)
    * Dual number class added
    
    * Moved file
    
    * Added basic tests
    
    * Added rvalues
    
    * Moved dualnumber tests
    
    * lint
    
    * Added support for pow
    
    * Added forward autograd
    
    * Changed type
    
    * multiplication wrong
    
    * added tests

---
+++ num_opt/autograd/dualnumber.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from numpy import log
+
+
+class DualNumber:
+    def __init__(self, a: float = 0., b: float = 0.):
+        '''
+        Dual number of form a + b\epsilon
+        '''
+        self.a = a
+        self.b = b
+
+    def __inverse(self):
+        # Inverse of the dual number, unstable if a is very small
+        return DualNumber(1 / self.a, -self.b / (self.a * self.a))
+
+    def __add__(self, other):
+        if isinstance(other, DualNumber):
+            return DualNumber(self.a + other.a, self.b + other.b)
+        return DualNumber(self.a + other, self.b)
+
+    __radd__ = __add__
+
+    def __div__(self, other):
+        if isinstance(other, DualNumber):
+            return self * other.__inverse()
+        return DualNumber(self.a / other, self.b)
+
+    def __rdiv__(self, other):
+        return DualNumber(other, 0.) / self
+
+    def __mul__(self, other):
+        if isinstance(other, DualNumber):
+            new_a = self.a * other.a
+            new_b = self.b * other.a + self.a * other.b
+            return DualNumber(new_a, new_b)
+        return DualNumber(self.a * other, self.b * other)
+
+    __rmul__ = __mul__
+
+    def __neg__(self):
+        return DualNumber(-self.a, -self.b)
+
+    def __pow__(self, other):
+        if isinstance(other, DualNumber):
+            new_a = self.a ** other.a
+            new_b = other.a * (self.a ** (other.a - 1)) * \
+                b + log(self.a) * new_a * other.b
+            return DualNumber(new_a, new_b)
+        return DualNumber(self.a ** other,
+                          other * (self.a ** (other - 1) * self.b))
+
+    def __rpow__(self, other):
+        return DualNumber(other, 0.) ** self
+
+    def __repr__(self):
+        return "D({:2f}, {:2f})".format(self.a, self.b)
+
+    def __sub__(self, other):
+        if isinstance(other, DualNumber):
+            return DualNumber(self.a - other.a, self.b - other.b)
+        return DualNumber(self.a - other, self.b)
+
+    def __rsub__(self, other):
+        return DualNumber(other, 0.) - self
---
+++ num_opt/tests/autograd.py
@@ -0,0 +1,56 @@
+import unittest
+from .util import check_float, parabola, multivar
+
+from ..autograd.dualnumber import DualNumber
+from ..autograd.forward import grad as fgrad
+
+DUAL_1 = DualNumber(1.0, 2.0)
+DUAL_2 = DualNumber(-3.1, 4.0)
+DUAL_3 = DualNumber(2., 6.)
+
+class DualNumberTest(unittest.TestCase):
+    def test_add_sub(self):
+        add = DUAL_1 + DUAL_2
+        self.assertTrue(check_float(add.a, -2.1))
+        self.assertTrue(check_float(add.b, 6))
+
+        sub = DUAL_1 - DUAL_2
+        self.assertTrue(check_float(sub.a, 4.1))
+        self.assertTrue(check_float(sub.b, -2.0))
+
+    def test_mult(self):
+        mult = DUAL_1 * DUAL_2
+        self.assertTrue(check_float(mult.a, -3.1))
+        self.assertTrue(check_float(mult.b, -2.2))
+        mult = DUAL_2 * DUAL_3
+        self.assertTrue(check_float(mult.a, -6.2))
+        self.assertTrue(check_float(mult.b, -10.6))
+        
+        inverse_check = DUAL_3 * DUAL_3._DualNumber__inverse()
+        self.assertTrue(check_float(inverse_check.a, 1))
+        self.assertTrue(check_float(inverse_check.b, 0))
+    
+    def test_float(self):
+        mult = 3.1 * DUAL_1
+        self.assertTrue(check_float(mult.a, 3.1))
+        self.assertTrue(check_float(mult.b, 6.2))
+
+        mult = 14 + DUAL_3
+        self.assertTrue(check_float(mult.a, 16))
+        self.assertTrue(check_float(mult.b, 6))
+
+class ForwardAutoTest(unittest.TestCase):
+    def test_parabola(self):
+        g = fgrad(parabola.py_func)
+        self.assertTrue(check_float(g(1), -18))
+        self.assertTrue(check_float(g(10), 0))
+        self.assertTrue(check_float(g(11.3), 2.6))
+    
+    def test_multi(self):
+        g = fgrad(multivar.py_func)
+        g_x, g_y = g(0, 1)
+        self.assertTrue(check_float(g_x, 11))
+        self.assertTrue(check_float(g_y, 1))
+        g_x, g_y = g(-1.3, 4.2)
+        self.assertTrue(check_float(g_x, 11.6))
+        self.assertTrue(check_float(g_y, -0.3))
