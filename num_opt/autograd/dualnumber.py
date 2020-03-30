589d612cad88a813f0a5fbf4d89311ee41540ee1
Neil Liu <neil.liu16@gmail.com>
2020-03-29 22:31:19 -0400

    Moved file

--- num_opt/autograd/dualnumber.py
+++ num_opt/autograd/dualnumber.py
@@ -6,24 +6,24 @@ class DualNumber:
         '''
         self.a = a
         self.b = b
-    
+
     def __inverse(self, other: DualNumber):
         # Inverse of the dual number
-        return DualNumber(1/self.a, -self.b/(self.a * self.a))
-    
+        return DualNumber(1 / self.a, -self.b / (self.a * self.a))
+
     def __add__(self, other: DualNumber):
         return DualNumber(self.a + other.a, self.b + other.b)
-    
+
     def __div__(self, other: DualNumber):
-        return self / other.__inverse()
-    
+        return self * other.__inverse()
+
     def __mul__(self, other: DualNumber):
-        return DualNumber(self.a * other.a, self.b * other.a + self.a * other.b)
-    
+        new_a = self.a * other.a
+        new_b = self.b * other.a + self.a * other.b
+        return DualNumber(new_a, new_b)
+
     def __neg__(self):
         return DualNumber(-self.a, -self.b)
-    
+
     def __sub__(self, other: DualNumber):
         return DualNumber(self.a - other.a, self.b - other.b)
-    
-
